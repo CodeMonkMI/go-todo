6c424b6f7765f21e8eb5fe7cac98f4e1b027022f
Monirul Islam <mmislam027@gmail.com>
2025-04-14 17:50:57 +0600

    update and delete operation added

--- main.go
+++ main.go
@@ -37,6 +37,10 @@ type (
 		Completed bool          `bson:"completed"`
 		CreatedAt time.Time     `bson:"createdAt"`
 	}
+	updateTodoModel struct {
+		Title     string `bson:"title"`
+		Completed bool   `bson:"completed"`
+	}
 	todo struct {
 		ID        string    `json:"id"`
 		Title     string    `json:"title"`
@@ -78,6 +82,7 @@ func fetchTodos(w http.ResponseWriter, r *http.Request) {
 		"data": formatTodos,
 	})
 }
+
 func createTodo(w http.ResponseWriter, r *http.Request) {
 	var newTodo todoModel
 	err := json.NewDecoder(r.Body).Decode(&newTodo)
@@ -126,8 +131,119 @@ func createTodo(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func updateTodo(w http.ResponseWriter, r *http.Request) {
+func fetchSingleTodo(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
 
+	objectId, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		responseError(w, err, "Invalid todo id")
+		return
+	}
+	var todoData todoModel
+	err2 := todoCollection.FindOne(context.TODO(), bson.D{{Key: "_id", Value: objectId}}).Decode(&todoData)
+	if err2 != nil {
+		responseError(w, err2, "Invalid todo id")
+		return
+	}
+	formatTodo := todo{
+		ID:        todoData.ID.Hex(),
+		Title:     todoData.Title,
+		Completed: todoData.Completed,
+		CreatedAt: todoData.CreatedAt,
+	}
+
+	rnd.JSON(w, http.StatusOK, renderer.M{
+		"data": formatTodo,
+	})
+}
+
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	objectId, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		responseError(w, err, "Invalid todo id")
+		return
+	}
+	var findTodo todoModel
+	err2 := todoCollection.FindOne(context.TODO(), bson.D{{Key: "_id", Value: objectId}}).Decode(&findTodo)
+	if err2 != nil {
+		responseError(w, err2, "Invalid todo id")
+		return
+	}
+
+	var updateTodo updateTodoModel
+	err3 := json.NewDecoder(r.Body).Decode(&updateTodo)
+	if err3 != nil {
+		responseError(w, err3, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if updateTodo.Title == "" {
+		updateTodo.Title = findTodo.Title
+	}
+	if !updateTodo.Completed {
+		updateTodo.Completed = findTodo.Completed
+	}
+
+	filter := bson.D{{Key: "_id", Value: objectId}}
+	update := bson.D{{Key: "$set", Value: updateTodo}}
+
+	_, err4 := todoCollection.UpdateOne(context.TODO(), filter, update)
+	if err4 != nil {
+		responseError(w, err4, "Failed to update todo")
+		return
+	}
+
+	rnd.JSON(w, http.StatusAccepted, renderer.M{
+		"message": "Todo updated successfully",
+		"data": todo{
+			ID:        findTodo.ID.Hex(),
+			Title:     updateTodo.Title,
+			Completed: updateTodo.Completed,
+			CreatedAt: findTodo.CreatedAt,
+		},
+	})
+}
+func deleteTodo(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	objectId, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		responseError(w, err, "Invalid todo id")
+		return
+	}
+	var findTodo todoModel
+	err2 := todoCollection.FindOne(context.TODO(), bson.D{{Key: "_id", Value: objectId}}).Decode(&findTodo)
+	if err2 != nil {
+		responseError(w, err2, "Invalid todo id")
+		return
+	}
+
+	filter := bson.D{{Key: "_id", Value: objectId}}
+
+	_, err3 := todoCollection.DeleteOne(context.TODO(), filter)
+
+	if err3 != nil {
+		responseError(w, err3, "Todo deletion failed")
+		return
+	}
+
+	rnd.JSON(w, http.StatusNoContent, renderer.M{
+		"message": "Todo delete successfully",
+		"data":    nil,
+	})
+}
+
+func todoHandlers() http.Handler {
+	r := chi.NewRouter()
+	r.Get("/", fetchTodos)
+	r.Post("/", createTodo)
+	r.Get("/{id}", fetchSingleTodo)
+	r.Patch("/{id}", updateTodo)
+	r.Delete("/{id}", deleteTodo)
+	return r
 }
 
 func main() {
@@ -189,15 +305,8 @@ func main() {
 
 }
 
-func todoHandlers() http.Handler {
-	r := chi.NewRouter()
-	r.Get("/", fetchTodos)
-	r.Post("/", createTodo)
-	r.Put("/{id}", updateTodo)
-	return r
-}
-
 func responseError(w http.ResponseWriter, err error, msg string) {
+	log.Println(err)
 	rnd.JSON(w, http.StatusBadRequest, renderer.M{
 		"message": msg,
 		"err":     err,
